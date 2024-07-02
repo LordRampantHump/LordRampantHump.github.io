3bcda7e280e6fc93c03e140ef8846f59da656d18
Lordrampanthump <lordrampenthump@gmail.com>
2024-07-02 17:48:20 +0100

    update build

--- react/twitch_ui/vite.config.ts
+++ react/twitch_ui/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: `https://lordrampanthump.github.io/react/twitch_ui/build/`,
+  server: {
+    proxy: {
+      '/': {
+        target: 'https://lordrampanthump.github.io/react/twitch_ui/build/'
+      }
+    }
+  }
 })
